55157dc9abe47092449d5588078a6bce5a0225db
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-04-29 21:02:23 -0400

    create base array

---
+++ src/utilities/array.tsx
@@ -0,0 +1,52 @@
+
+const sanrioArray = [{
+    name:'Aggretsuko',
+    src:'src/imgs/aggretsuko.webp',
+    clicked:false
+},{
+    name:'Badtz-Maru',
+    src:'src/imgs/badtz_maru.webp',
+    clicked:false
+},{
+    name:'Chococat',
+    src:'src/imgs/chococat.webp',
+    clicked:false
+},{
+    name:'Cinnamoroll',
+    src:'src/imgs/cinnamoroll.webp',
+    clicked:false
+},{
+    name:'Hello Kitty',
+    src:'src/imgs/hello_kitty.webp',
+    clicked:false
+},{
+    name:'Keroppi',
+    src:'src/imgs/keroppi.webp',
+    clicked:false
+},{
+    name:'Kuromi',
+    src:'src/imgs/kuromi.webp',
+    clicked:false
+},{
+    name:'Little Twin Stars',
+    src:'src/imgs/little_twin_stars.webp',
+    clicked:false
+},{
+    name:'My Melody',
+    src:'src/imgs/my_melody.webp',
+    clicked:false
+},{
+    name:'Pochacco',
+    src:'src/imgs/pochacco.webp',
+    clicked:false
+},{
+    name:'Pompompurin',
+    src:'src/imgs/pompompurin.webp',
+    clicked:false
+},{
+    name:'Tuxedosam',
+    src:'src/imgs/tuxedosam.webp',
+    clicked:false
+}]
+
+export default sanrioArray
