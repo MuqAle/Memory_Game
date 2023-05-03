fc3cd9f51ff347d128c2a9a376a8e89bcc0c2541
Muqsiteen Aleema <muqsiteen.a@gmail.com>
2023-05-02 23:25:30 -0400

    add color

--- src/utilities/array.tsx
+++ src/utilities/array.tsx
@@ -2,51 +2,63 @@
 const sanrioArray = [{
     name:'Aggretsuko',
     src:'src/imgs/aggretsuko.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(250, 217, 98)'
 },{
     name:'Badtz-Maru',
     src:'src/imgs/badtz_maru.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(161, 221, 240)'
 },{
     name:'Chococat',
     src:'src/imgs/chococat.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(201, 146, 220)'
 },{
     name:'Cinnamoroll',
     src:'src/imgs/cinnamoroll.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(161, 221, 240)'
 },{
     name:'Hello Kitty',
     src:'src/imgs/hello_kitty.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(255, 192, 221)'
 },{
     name:'Keroppi',
     src:'src/imgs/keroppi.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(116, 208, 124)'
 },{
     name:'Kuromi',
     src:'src/imgs/kuromi.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(201, 146, 220)'
 },{
     name:'Little Twin Stars',
     src:'src/imgs/little_twin_stars.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(161, 221, 240)'
 },{
     name:'My Melody',
     src:'src/imgs/my_melody.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(255, 192, 221)'
 },{
     name:'Pochacco',
     src:'src/imgs/pochacco.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(201, 146, 220)'
 },{
     name:'Pompompurin',
     src:'src/imgs/pompompurin.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(250, 217, 98)'
 },{
     name:'Tuxedosam',
     src:'src/imgs/tuxedosam.webp',
-    clicked:false
+    clicked:false,
+    color:'rgb(161, 221, 240)'
 }]
 
 export default sanrioArray
